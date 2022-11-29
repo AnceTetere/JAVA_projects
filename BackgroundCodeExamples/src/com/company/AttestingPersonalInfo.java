944cf2a5afa95337820cd27bbf7c8b4881569022
AnceTetere <ance.tetere@outlook.com>
2022-11-29 23:22:10 +0200

    The initial blueprint for attesting one's personal info.

--- BackgroundCodeExamples/src/com/company/AttestingPersonalInfo.java
+++ BackgroundCodeExamples/src/com/company/AttestingPersonalInfo.java
@@ -4,43 +4,19 @@ import java.util.Scanner;
 
 public class AttestingPersonalInfo {
 
-    public static int checkData(int currentYear, String dateOfBirth) {
-        int dob = Integer.parseInt(dateOfBirth);
-        int minimumYear = currentYear - 125;
-
-        if ((dob < minimumYear) || (dob > currentYear)) {
-            return -1;
-        }
-        return (currentYear - dob);
-    }
-
     public static String getInputFromScanner(int currentYear) {
+
         Scanner scanner = new Scanner(System.in);
 
-//        String name = System.console().readLine("Hi, what's your name? ");
         System.out.println("Hi, what's your name? ");
         String name = scanner.nextLine();
 
-        System.out.println("Hi " + name + ", thanks for your interest!");
-//        String dateOfBirth = System.console().readLine("What year were you born? ");
+        System.out.println("Hi " + name + ", thankd for your interest.");
         System.out.println("What year were you born? ");
+        String dateOfBirth = scanner.nextLine();
 
-        boolean validDOB = false;
-        int age = 0;
-
-        do {
-            System.out.println("Enter a year of birth >= " + (currentYear - 125) + " and <= " + (currentYear));
-            try {
-//            String dateOfBirth = scanner.nextLine();
-//            age = currentYear - Integer.parseInt(dateOfBirth);
-                age = checkData(currentYear, scanner.nextLine());
-                validDOB = age < 0 ? false : true;
-            } catch (NumberFormatException badUserData) {
-                System.out.println("Characters not allowed! Please, try again.");
-            }
-        } while (!validDOB);
-        return "So you are " + age + " years old";
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+        return "You are " + age + " years old";
     }
-
-
 }
+
