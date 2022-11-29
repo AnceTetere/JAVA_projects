9acb90c3ad8b603266ee26a761d49939826f6d2b
AnceTetere <ance.tetere@outlook.com>
2022-11-30 00:35:46 +0200

    Update to the PromptingUser class
    Allowing for the double data types, as well as setting up some initial checks for letters.

--- BackgroundCodeExamples/src/com/company/PromptingUser.java
+++ BackgroundCodeExamples/src/com/company/PromptingUser.java
@@ -8,13 +8,14 @@ public class PromptingUser {
         Scanner scanner = new Scanner(System.in);
 
         int counter = 1;
-        int sum = 0;
+        double sum = 0;
 
         while (counter <= 5) {
             System.out.println("Enter number #" + counter + ":");
             String nextNumber = scanner.nextLine();
             try {
-                int number = Integer.parseInt(nextNumber);
+//                int number = Integer.parseInt(nextNumber);
+                double number = Double.parseDouble(nextNumber);
                 counter++;
                 sum += number;
             } catch (NumberFormatException nfe) {
@@ -22,7 +23,6 @@ public class PromptingUser {
             }
 
         }
-
-
+        System.out.println("The sum of all 5 numbers is " + sum);
     }
 }
