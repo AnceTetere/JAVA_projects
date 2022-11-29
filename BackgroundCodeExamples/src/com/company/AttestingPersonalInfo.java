264aeccb7cdf0e024040678260d42094c758fc25
AnceTetere <ance.tetere@outlook.com>
2022-11-30 00:16:03 +0200

    Addind the AttestingPersonalInfo class.

--- BackgroundCodeExamples/src/com/company/AttestingPersonalInfo.java
+++ BackgroundCodeExamples/src/com/company/AttestingPersonalInfo.java
@@ -11,12 +11,32 @@ public class AttestingPersonalInfo {
         System.out.println("Hi, what's your name? ");
         String name = scanner.nextLine();
 
-        System.out.println("Hi " + name + ", thankd for your interest.");
+        System.out.println("Hi " + name + ", thank you for your interest.");
         System.out.println("What year were you born? ");
-        String dateOfBirth = scanner.nextLine();
+        boolean validDOB = false;
+        int age = 0;
 
-        int age = currentYear - Integer.parseInt(dateOfBirth);
+        do {
+            System.out.println("Enter a year of birth >= " + (currentYear - 125) + " and <= " + (currentYear));
+            try {
+                age = checkData(currentYear, scanner.nextLine());
+                validDOB = age < 0 ? false : true;
+            } catch (NumberFormatException badUserData) {
+                System.out.println("Letters not allowed. Please, try again.");
+            }
+        } while (!validDOB);
+
         return "You are " + age + " years old";
+    }
+
+    public static int checkData(int currentYear, String dateOfBirth) {
+        int dob = Integer.parseInt(dateOfBirth);
+        int minimumYear = currentYear - 125;
+
+        if ((dob < minimumYear) || (dob > currentYear)) {
+            return -1;
+        }
+        return (currentYear - dob);
     }
 }
 
