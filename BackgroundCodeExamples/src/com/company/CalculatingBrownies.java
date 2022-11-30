0d65f2ea8e8a2c459585404496f3e06677763051
AnceTetere <ance.tetere@outlook.com>
2022-12-01 01:40:07 +0200

    Calculating customer awards.
    Methods overloaded.

---
+++ BackgroundCodeExamples/src/com/company/CalculatingBrownies.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class CalculatingBrownies {
+    public static int calculateBrowniePoints(String customerName, int points) {
+        System.out.println("Client, " + customerName + " has received " + points + " points");
+        return points * 1000;
+    }
+
+    public static int calculateBrowniePoints(int points) {
+        System.out.println("Unnamed client has received " + points + " points");
+        return points * 1000;
+    }
+
+    public static int calculateBrowniePoints() {
+        System.out.println("No client, no points");
+        return 0;
+    }
+}
