244bd6b67ecf17c0ca26e78f52c779ac35be4a4d
AnceTetere <ance.tetere@outlook.com>
2022-12-03 23:40:09 +0200

    Brainstorming for setting values

---
+++ BackgroundCodeExamples/src/com/company/SettingValues.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+public class SettingValues {
+    public static void settingValues() {
+        for (int number = 1; number < 7; number += 2) {
+            System.out.println("number = " + number);
+        }
+        System.out.println("=====================");
+        for (int number = 1; number < 5; number += 2) {
+            System.out.println("number = " + number);
+        }
+
+        System.out.println("=====================");
+        for (int number = 100; number < 130; number += 10) {
+            System.out.println("number = " + number);
+        }
+    }
+}
